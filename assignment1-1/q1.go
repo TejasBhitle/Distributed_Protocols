2c152980afa7f05bef91e1cb68accfa3507111ed
TejasBhitle <bhitletejas@gmail.com>
2022-09-02 15:53:01 -0400

    Q1 done

--- assignment1-1/q1.go
+++ assignment1-1/q1.go
@@ -2,14 +2,20 @@ package cos418_hw1_1
 
 import (
 	"fmt"
+	"log"
+	"os"
+	"regexp"
 	"sort"
+	"strings"
 )
 
 // Find the top K most common words in a text document.
-// 	path: location of the document
+//
+//	path: location of the document
 //	numWords: number of words to return (i.e. k)
 //	charThreshold: character threshold for whether a token qualifies as a word,
 //		e.g. charThreshold = 5 means "apple" is a word but "pear" is not.
+//
 // Matching is case insensitive, e.g. "Orange" and "orange" is considered the same word.
 // A word comprises alphanumeric characters only. All punctuation and other characters
 // are removed, e.g. "don't" becomes "dont".
@@ -18,7 +24,39 @@ func topWords(path string, numWords int, charThreshold int) []WordCount {
 	// TODO: implement me
 	// HINT: You may find the `strings.Fields` and `strings.ToLower` functions helpful
 	// HINT: To keep only alphanumeric characters, use the regex "[^0-9a-zA-Z]+"
-	return nil
+
+	// Step 1 : Reading file into string and then into words array
+	byteArr, file_err := os.ReadFile(path)
+	if file_err != nil {
+		log.Fatal(file_err)
+	}
+	words := strings.Fields(string(byteArr))
+	// fmt.Println(words)
+
+	// Step 2: Filtering
+	alphaNumericRegex, reg_error := regexp.Compile(`[^0-9a-zA-Z]+`)
+	if reg_error != nil {
+		log.Fatal(reg_error)
+	}
+
+	wordsMap := make(map[string]int)
+
+	for _, word := range words {
+		word = alphaNumericRegex.ReplaceAllString(word, "")
+		if len(word) >= charThreshold {
+			key := strings.ToLower(word)
+			wordsMap[key]++
+		}
+	}
+
+	wordCounts := []WordCount{}
+	for key, value := range wordsMap {
+		wordCounts = append(wordCounts, WordCount{Word: key, Count: value})
+	}
+
+	sortWordCounts(wordCounts)
+
+	return wordCounts[:numWords] // getting the top k words
 }
 
 // A struct that represents how many times a word is observed in a document
@@ -43,4 +81,8 @@ func sortWordCounts(wordCounts []WordCount) {
 		}
 		return wc1.Count > wc2.Count
 	})
+}
+
+func main() {
+	topWords("simple.txt", 4, 0)
 }
