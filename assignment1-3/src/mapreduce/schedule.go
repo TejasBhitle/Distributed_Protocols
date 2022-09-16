2352be3c53e82c91d5daca9eab66c8989d9d2653
TejasBhitle <bhitletejas@gmail.com>
2022-09-16 14:48:29 -0400

    [assignment1-3] untested scheduler code written, RPC Server start issue

--- assignment1-3/src/mapreduce/schedule.go
+++ assignment1-3/src/mapreduce/schedule.go
@@ -1,5 +1,10 @@
 package mapreduce
 
+import (
+	"fmt"
+	"sync"
+)
+
 // schedule starts and waits for all tasks in the given phase (Map or Reduce).
 func (mr *Master) schedule(phase jobPhase) {
 	var ntasks int
@@ -22,5 +27,43 @@ func (mr *Master) schedule(phase jobPhase) {
 	//
 	// TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO
 	//
+
+	// Add jobs to pendingTasks
+	pendingTasksChan := make(chan DoTaskArgs)
+	for i := 0; i < ntasks; i++ {
+		pendingTasksChan <- DoTaskArgs{
+			JobName:       mr.jobName,
+			File:          mr.files[i],
+			Phase:         phase,
+			TaskNumber:    i,
+			NumOtherPhase: nios,
+		}
+	}
+
+	var waitGroup sync.WaitGroup
+	waitGroup.Add(ntasks)
+
+	// listening to workers channel for availability
+	go func() {
+		for {
+			workerName := <-mr.registerChannel
+			taskArgs := <-pendingTasksChan
+
+			ok := call(workerName, "Worker.DoTask", taskArgs, nil)
+			if ok == false {
+				// task is not done. add it back to pending
+				pendingTasksChan <- taskArgs
+
+			} else {
+				// task is complete
+				waitGroup.Done()
+			}
+		}
+	}()
+
+	// Waiting for all the jobs to execute
+	waitGroup.Wait()
+	fmt.Println("startScheduler finished " + phase)
+
 	debug("Schedule: %v phase done\n", phase)
 }
